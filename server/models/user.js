521ef6a0aeedd39d70be66e96ca60d4954199728
sinikala <46318664+sinikala@users.noreply.github.com>
2022-04-19 12:44:11 +0300

    server end login

--- server/models/user.js
+++ server/models/user.js
@@ -4,12 +4,12 @@ const userSchema = new mongoose.Schema({
   username: String,
   name: String,
   passwordHash: String,
-  // notes: [
-  //   {
-  //     type: mongoose.Schema.Types.ObjectId,
-  //     ref: 'Note'
-  //   }
-  // ],
+  notes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Note'
+    }
+  ],
 })
 
 userSchema.set('toJSON', {
