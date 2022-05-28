60297cb0312a3728f7901c69cd47b822b0b0013d
sinikala <46318664+sinikala@users.noreply.github.com>
2022-05-28 15:48:45 +0300

    Add more comments to visited park (backend)

--- server/controllers/visitedParks.js
+++ server/controllers/visitedParks.js
@@ -64,4 +64,50 @@ visitedParkRouter.post('/', async (request, response) => {
   response.status(201).json(savedVisitedPark)
 })
 
+visitedParkRouter.put('/comment/:id', async (request, response) => {
+  const { comment } = request.body
+
+  let decodedToken = ''
+
+  try {
+    decodedToken = tokenIsValid(request)
+    if (!decodedToken.id) {
+      return response.status(401).json({
+        error: 'token missing or invalid'
+      })
+    }
+  } catch {
+    return response.status(401).json({
+      error: 'token expired'
+    })
+  }
+  //const user = await User.findById(decodedToken.id)
+  const oldVisitedPark = await VisitedPark.findById(request.params.id)
+  const updatedComments = oldVisitedPark.comments.concat(
+    { createdAt: new Date(), comment: comment }
+  )
+  try {
+    const updated = await VisitedPark.findOneAndUpdate(
+      request.params.id, { comments: updatedComments }, { new: true }
+    )
+    response.json(updated)
+  } catch {
+    console.log('error')
+  }
+  // const updatedVisitedPark = new VisitedPark({
+  //   park: oldVisitedPark.park,
+  //   visitedIn: oldVisitedPark.visitedIn,
+  //   createdAt: oldVisitedPark.createdAt,
+  //   comments: oldVisitedPark.comments.concat({ createdAt: new Date(), comment: comment }),
+  //   user: user._id,
+  //   routes: oldVisitedPark.routes
+  // })
+
+  // VisitedPark.findByIdAndUpdate(request.params.id, updatedVisitedPark, { new: true })
+  //   .then(updatedVisitedPark => {
+  //     response.json(updatedVisitedPark)
+  //   })
+  //   .catch(error => console.log(error))
+})
+
 module.exports = visitedParkRouter
