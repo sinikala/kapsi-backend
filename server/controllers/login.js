77278da4878dbd19ed854e00e7990bcdf1296b2d
sinikala <46318664+sinikala@users.noreply.github.com>
2022-05-29 14:37:47 +0300

    Edit token expiration time

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -23,7 +23,7 @@ loginRouter.post('/', async (request, response) => {
     id: user._id,
   }
 
-  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: 60 * 60 })
+  const token = jwt.sign(userForToken, process.env.SECRET, { expiresIn: '3h' })
 
   response
     .status(200)
